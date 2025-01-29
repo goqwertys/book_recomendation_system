59a9e73372d59cf4ddb0ee312fc1e4483e9f192e
georgii <goqwertys@gmail.com>
2025-01-30 02:13:23 +0400

    settings.CACHES set up

--- config/settings.py
+++ config/settings.py
@@ -110,3 +110,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': os.getenv('LOCATION'),
+    }
+}

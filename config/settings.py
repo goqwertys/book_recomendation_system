55f829155c6af754763f4a2079f244ae77b406c1
georgii <goqwertys@gmail.com>
2025-01-30 02:04:50 +0400

    DB set up

--- config/settings.py
+++ config/settings.py
@@ -71,11 +71,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB'),
+            'USER': os.getenv('POSTGRES_USER'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+            'HOST': os.getenv('POSTGRES_HOST'),
+            'PORT': os.getenv('POSTGRES_PORT'),
+        }
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {

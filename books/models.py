f3dc29dec2a07d394650d44e49baaba6614f89ab
georgii <goqwertys@gmail.com>
2025-01-31 14:25:16 +0400

    Genre, Author, Book, Interaction models added

--- books/models.py
+++ books/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
+
+class Genre(models.Model):
+    """ Genre model """
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    """ Author model """
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+    class Meta:
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """ Book model """
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    description = models.TextField(**NULLABLE)
+    cover = models.ImageField(
+        upload_to='books/'
+    )
+    genres = models.ManyToManyField('Genre')
+    publish_data = models.DateField()
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+
+    def __str__(self):
+        return self.title
--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+import books.models
+import users.models
+
+
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
+
+class Interaction(models.Model):
+    """ Interaction model """
+    user = models.ForeignKey(users.models.User, on_delete=models.CASCADE)
+    book = models.ForeignKey(books.models.Book, on_delete=models.CASCADE)
+
+    rating = models.FloatField(
+        **NULLABLE,
+        validators=[MinValueValidator(1.0), MaxValueValidator(5.0)]
+    )
+    viewed = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Interaction'
+        verbose_name_plural = 'Interactions'
+        unique_together = (('user', 'book'),)
+        indexes = [
+            models.Index(fields=['user', 'book'])
+        ]

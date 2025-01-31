fbe1667f2398c1f92c4f3e49c7dab1af365fa2a2
georgii <goqwertys@gmail.com>
2025-01-31 16:31:15 +0400

    Interaction registered in interactions.admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from interactions.models import Interaction
+
+
+@admin.register(Interaction)
+class InteractionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'rating', 'viewed', 'timestamp')
+    list_filter = ('viewed', 'rating')
+    search_fields = ('user__username', 'book__title')
+    ordering = ('-timestamp',)

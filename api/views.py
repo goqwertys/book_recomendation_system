34cf6c5273e5388066d9b24255b519065fe9e9a0
georgii <goqwertys@gmail.com>
2025-01-31 14:53:21 +0400

    ViewSets added to api/views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.serializers import GenreSerializer, AuthorSerializer, BookSerializer, InteractionSerializer
+from books.models import Author, Genre, Book
+from interactions.models import Interaction
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class InteractionViewSet(viewsets.ModelViewSet):
+    queryset = Interaction.objects.all()
+    serializer_class = InteractionSerializer

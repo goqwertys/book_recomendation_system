4552aadb8b5a4192b0fa5a482e1730e71f36017e
georgii <goqwertys@gmail.com>
2025-02-01 19:26:50 +0400

    Web auth urls updated

---
+++ users/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import RegisterView, LoginView, LogoutView, ProfileView, ProfileUpdateView
+
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('<int:pk>/', ProfileView.as_view(), name='profile'),
+    path('profile/', ProfileView.as_view(), name='my-profile'),
+    path('edit/', ProfileUpdateView.as_view(), name='edit-profile'),
+]

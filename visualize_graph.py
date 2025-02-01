6c548092a0eb611283b1862d7c10c8537a2ec1fc
georgii <goqwertys@gmail.com>
2025-02-02 02:58:56 +0400

    parameters added to parser

--- visualize_graph.py
+++ visualize_graph.py
@@ -10,23 +10,49 @@ from recommendations.visualization import visualize_interactive_graph
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Visualize user-book interactions graph")
+    parser = argparse.ArgumentParser(
+        description="Visualize user-book interactions graph",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter  # Shows default values
+    )
+
+    # Basic filtering parameters
     parser.add_argument(
         "--max_users",
         type=int,
         default=30,
-        help="Maximum number of users to include (default: 30)"
+        help="Maximum number of most active users to include (default: %(default)s)"
     )
+    parser.add_argument(
+        "--max_books",
+        type=int,
+        default=50,
+        help="Maximum number of most popular books to include (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--min_interactions",
+        type=int,
+        default=1,
+        dest="min_book_interactions",
+        help="Minimum interactions required to include a book (default: %(default)s)"
+    )
+
+    # Output options
     parser.add_argument(
         "--output",
         type=str,
         default="graph.html",
-        help="Output filename (will be saved in recommendations/visualizations/saved_graphs/)"
+        help="Output filename (saved to recommendations/visualizations/saved_graphs/)"
     )
 
     args = parser.parse_args()
 
-    G = build_filtered_graph(max_users=args.max_users)
+    # We pass all parameters to the graph construction function
+    G = build_filtered_graph(
+        max_users=args.max_users,
+        max_books=args.max_books,
+        min_book_interactions=args.min_book_interactions
+    )
+
     visualize_interactive_graph(G, args.output)
 
 

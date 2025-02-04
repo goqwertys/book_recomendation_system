f5096c819ab7ed99ecf16d18db1773452a360125
georgii <goqwertys@gmail.com>
2025-02-04 22:12:02 +0400

    UserRegisterForm updated/ register.html updated

--- users/forms.py
+++ users/forms.py
@@ -1,24 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 
+import books
+from books.forms import StyleFormMixin
 from books.models import Genre
 from users.models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    avatar = forms.ImageField(required=False, label='Avatar')
     preferred_genres = forms.ModelMultipleChoiceField(
-        queryset=Genre.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False
+        queryset=books.models.Genre.objects.all(),
+        required=False,
+        label='Preferred Genres'
     )
-
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2', 'avatar', 'preferred_genres']
-        labels = {
-            'email': 'Email',
-            'avatar': 'Avatar'
-        }
+        fields = ('email', 'password1', 'password2', 'avatar', 'preferred_genres')
 
 
 class UserLoginForm(AuthenticationForm):

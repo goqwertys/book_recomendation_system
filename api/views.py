99767a0bc911a5f0b16ad5958a65730e604595fc
georgii <goqwertys@gmail.com>
2025-02-04 04:40:50 +0400

    fiters added to viewsets

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets, generics, permissions, mixins
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, generics, permissions, mixins, filters
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
 
+from api.filters import AuthorFilter, GenreFilter, BookFilter
 from api.paginators import AuthorPaginator, GenrePaginator, BookPaginator, InteractionPaginator, UserPaginator
 from api.permissions import IsHimself
 from api.serializers import GenreSerializer, AuthorSerializer, BookSerializer, InteractionSerializer, UserSerializer, \
@@ -18,24 +20,41 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     pagination_class = AuthorPaginator
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_class = AuthorFilter
+    ordering_fields = ['name']
+    search_fields = ['name', 'bio']
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     pagination_class = GenrePaginator
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_class = GenreFilter
+    ordering_fields = ['name']
+    search_fields = ['name']
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     pagination_class = BookPaginator
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_class = BookFilter
+    ordering_fields = ['average_rating', 'publish_date']
+    search_fields = ['name']
 
 
 class InteractionViewSet(viewsets.ModelViewSet):
     queryset = Interaction.objects.all()
     serializer_class = InteractionSerializer
     pagination_class = InteractionPaginator
+    def perform_create(self, serializer):
+        """
+        Automatically adds the current user to the `user` field.
+        """
+        serializer.save(user=self.request.user)
 
 
 class UserViewSet(viewsets.GenericViewSet,
@@ -69,11 +88,15 @@ class UserViewSet(viewsets.GenericViewSet,
         serializer = self.get_serializer(user)
         return Response(serializer.data)
 
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    filterset_fields = ['email']
+    ordering_fields = ['email']
+    search_fields = ['email', 'preferred_genres']
+
 
 class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = [permissions.AllowAny]
-
 
 
 @api_view(['GET'])

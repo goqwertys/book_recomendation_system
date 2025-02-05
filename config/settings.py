ba3b795194a56c44a2fbb4dadaae5b6b4eeb260b
georgii <goqwertys@gmail.com>
2025-02-06 01:39:50 +0400

    docs added to settings and config

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'drf_yasg',
+
     'users',
     'books',
     'interactions',
--- config/urls.py
+++ config/urls.py
@@ -18,12 +18,33 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Book recommendation service",
+        default_version='v0.1',
+        description="Rate and get recommendations",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="goqwertys@gmail.com", name="Georgii"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny,]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('users.urls', namespace='users')),
-    path('books/', include('books.urls', namespace='books'))
+    path('books/', include('books.urls', namespace='books')),
+
+    # docs
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
